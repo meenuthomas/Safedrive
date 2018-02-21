1a1ddd95d1d006b368818b931aa35a0566f9db28
meenuthomas <meenuthomas2013@gmail.com>
2018-02-21 14:59:33 +0530

    Delete Constants.java

--- Constants.java
+++
@@ -1,9 +0,0 @@
-package com.meenu.safedrive;
-
-
-
-public class Constants {
-    public static int TRAFFIC_ID= 1;
-    public static int SCHOOL_ID = 2;
-    public static String BASE_PATH="https://safedrive-b9651.firebaseio.com/";
-}
